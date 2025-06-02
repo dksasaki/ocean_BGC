ef4e534a17a992036fe629571134d0fdd9b6a599
Subhadeep Rakshit <Subhadeep.Rakshit@gaea56.ncrc.gov>
2025-06-02 14:04:11 -0400

    Troubleshooting

--- generic_tracers/generic_CBED.F90
+++ generic_tracers/generic_CBED.F90
@@ -62,7 +62,7 @@ type(generic_CBED_type) :: cbed
 
     ! grid param end. 
 
-    real,dimension(:,:)    :: w  !sedimentation rate
+    real,dimension(isd:ied,jsd:jed)    :: w  !sedimentation rate
 
 
 
@@ -272,7 +272,7 @@ contains
     real, dimension(:,:,:),       intent(in)    :: grid_tmask
     integer,                      intent(in)    :: isc,iec, jsc,jec, isd, jsd, nk
     integer, dimension(:,:),      intent(in)    :: mask_coast, grid_kmt    
-    real,dimension(isd:ied,jsd:jed), intent(out)    :: w  !sedimentation rate
+    real,                         intent(out)   :: w(isd:ied,jsd:jed)  !sedimentation rate
 
     integer :: i, j, k
     ! now write the calculation.
