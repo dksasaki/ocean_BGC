5bd3eb06fa0a70f1b710f335bebf5fba4b89d771
Subhadeep Rakshit <Subhadeep.Rakshit@gaea53.ncrc.gov>
2025-06-09 15:00:05 -0400

    Troubleshooting

--- generic_tracers/generic_CBED.F90
+++ generic_tracers/generic_CBED.F90
@@ -504,9 +504,9 @@ contains
          do j = jsc, jec; do i = isc, iec
                if (grid_kmt(i,j) .gt. 0) then
                   ! POC flux unit in umol cm-2 y-1. Unit of k is y-1
-                  k1(i,j) = (1.5*10**(-1))*(cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery) 
-                  k2(i,j) = (cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery)**(0.85) 
-                  k3(i,j) = (1.3*10**(-4))*(cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery)**(0.85)
+                  k1(i,j) = ( 0.15*(cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery)**(0.85) )/spery 
+                  k2(i,j) = ( 0.0023*(cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery)**(0.85) )/spery
+                  k3(i,j) = ( 0.00013*(cobalt%fntot_btm(i,j)*cobalt%c_2_n *1e6/1e4*spery)**(0.85) )/spery
                endif
          enddo;enddo 
          
