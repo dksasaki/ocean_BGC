1d7491950e52680d57815dd8603eaaabb4abe58a
Subhadeep Rakshit <Subhadeep.Rakshit@gaea51.ncrc.gov>
2026-03-05 16:52:57 -0500

    minor change

--- generic_tracers/generic_CBED.F90
+++ generic_tracers/generic_CBED.F90
@@ -23,7 +23,7 @@ module generic_CBED
    public generic_CBED_init, generic_CBED_end
    public generic_CBED_reg_diagnostics, generic_CBED_send_diagnostics
 
-   integer, parameter :: nk_cbed = 50    ! Number of benthic layers
+   integer, parameter :: nk_cbed = 20    ! Number of benthic layers
 
    type generic_CBED_type
       ! TODO: change read_porosity_from_file into a namelist variable
@@ -148,7 +148,7 @@ module generic_CBED
    ! grid
    ! local parameters
    real, parameter :: l_cbed = 0.20           ! length of sediment domain | sediment depth (m, 20 cm)
-   real, parameter :: dz1_cbed = 0.001        ! thickness of the first layer (m). For increasing thickness
+   real, parameter :: dz1_cbed = 0.003        ! thickness of the first layer (m). For increasing thickness
    real, parameter :: rho_s = 2.5             ! solid density (g/cm³)
    real, parameter :: Db_l = 0.08             ! bioturbation length scale (m) 8 cm.
    real, parameter :: bioirri_l = 0.018       ! bioirrigation length scale (m) 1.8 cm.
