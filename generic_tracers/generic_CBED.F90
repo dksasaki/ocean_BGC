4f579a15b2e728abdf0a6be672a5542108af4c69
Subhadeep Rakshit <Subhadeep.Rakshit@gaea56.ncrc.gov>
2026-04-30 10:02:32 -0400

    last failed. calc extra o2 uptake in b_o2.

--- generic_tracers/generic_CBED.F90
+++ generic_tracers/generic_CBED.F90
@@ -1219,7 +1219,9 @@ contains
             if (grid_kmt(i,j) .gt. 0) then
                b_o2(i,j) = por(i,j,1)*D_o2(i,j,1)*((cobalt%btm_o2(i,j)*cobalt%Rho_0 - c_o2(i,j,1))/(dz_cbed(1)/2.0)) + &
                   por(i,j,1)*w(i,j,1)*(cobalt%btm_o2(i,j)*cobalt%Rho_0) + &
-                  sum(dz_cbed(:)*por(i,j,1:nk_cbed)* bioirri(i,j,:)*(cobalt%btm_o2(i,j)*cobalt%Rho_0 - c_o2(i,j,:)) )
+                  sum(dz_cbed(:)*por(i,j,1:nk_cbed)* bioirri(i,j,:)*(cobalt%btm_o2(i,j)*cobalt%Rho_0 - c_o2(i,j,:)) ) - &
+                  por(i,j,1)*D_odu(i,j,1)*((0.0 - c_odu(i,j,1))/(dz_cbed(1)/2.0)) - &
+                  sum(dz_cbed(:)*por(i,j,1:nk_cbed)* bioirri(i,j,:)*(0.0 - c_odu(i,j,:)) )
 
                b_nh4(i,j) = por(i,j,1)*D_nh4(i,j,1)*((cobalt%f_nh4(i,j,nk)*cobalt%Rho_0 - c_nh4(i,j,1))/(dz_cbed(1)/2.0)) + &
                   por(i,j,1)*w(i,j,1)*(cobalt%f_nh4(i,j,nk)*cobalt%Rho_0) + &
@@ -2130,7 +2132,7 @@ contains
                   cbed_org_alk(i,j)
 
                !cobalt%b_dic(i,j) = b_dic(i,j)
-               cobalt%b_o2(i,j)  = b_o2(i,j) - b_odu(i,j)   ! + max(0.0, - (cbed%odu_flux(i,j)))  ! Add the ODU flux as added oxygen demand by the sediment because released ODU will be consummed in the bottom water.
+               cobalt%b_o2(i,j)  = b_o2(i,j)   ! + max(0.0, - (cbed%odu_flux(i,j)))  ! Add the ODU flux as added oxygen demand by the sediment because released ODU will be consummed in the bottom water.
                ! In absence of BW O2, it will create -ve O2 conc in BW. cbed%odu_flux(i,j) value is negative meaning efflux of ODU from sediment.
                ! Multiply with - sign will convert it to +ve meaning it will effectively "increase" b_o2 i.e. benthic oxygen demand.
 
