9950ed50ac8d7f146282555b061b7d8e8cd9e0db
dksasaki <dalton.sasaki@gmail.com>
2026-08-07 12:26:55 -0400

    including nan check

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5143,9 +5143,16 @@ contains
                                             cobalt%jremin_ndet_kelp(i,j) * cobalt%n_2_n_denit
                 cobalt%jprod_nh4_kelp(i,j) = cobalt%jprod_nh4_kelp(i,j) + cobalt%jremin_ndet_kelp(i,j)
 
-                if (isnan(cobalt%jprod_nh4_kelp(i,j))) then
+               if (isnan(cobalt%jprod_nh4_kelp(i,j))) then
                   write(*,*) 'NaN in jprod_nh4_kelp at i,j=',i,j
                   call mpp_error(FATAL,'NaN detected in jprod_nh4_kelp')
+               endif
+
+               if (isnan(cobalt%f_ndet_kelp(i,j))) then
+                  write(*,'(a,2i5,4es14.6)') 'NaN in f_ndet_kelp at i,j,jremin,jprod_nh4=', &
+                     i, j, cobalt%jremin_ndet_kelp(i,j), cobalt%jprod_nh4_kelp(i,j), &
+                     cobalt%f_ndet_kelp(i,j), cobalt%f_o2(i,j,k)
+                  call mpp_error(FATAL, 'NaN detected in f_ndet_kelp')
                endif
             endif
 
