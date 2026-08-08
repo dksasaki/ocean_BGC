308be93226e42a7327ce717d57d6c43b956699fd
dksasaki <dalton.sasaki@gmail.com>
2026-08-08 12:27:05 -0400

    ensuring carbon conservation

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5104,7 +5104,7 @@ contains
           cobalt%jo2resp_wc(i,j,k) = cobalt%jo2resp_wc(i,j,k) + &
 		        (cobalt%jremin_ndet(i,j,k) + cobalt%jremin_ndet_fast(i,j,k)) * cobalt%o2_2_nh4
 
-            if (cobalt%do_external_source .and. k .eq. grid_kmt(i,j)) then
+            if (cobalt%do_external_source .and. k .eq. grid_kmt(i,j) .and. mask_addition_t(i,j,1) .gt. 0.0) then
                 cobalt%jremin_ndet_kelp(i,j) = cobalt%gamma_ndet * cobalt%expkreminT(i,j,k) * &
                                         cobalt%f_o2(i,j,k) / ( cobalt%k_o2 + cobalt%f_o2(i,j,k) ) * &
                                         max(0.0, cobalt%f_ndet_kelp(i,j) * (1.0 - rp_kelp_agent))
@@ -5132,7 +5132,7 @@ contains
 		       (cobalt%jremin_ndet(i,j,k) + cobalt%jremin_ndet_fast(i,j,k)) * cobalt%n_2_n_denit
           cobalt%jprod_nh4(i,j,k) = cobalt%jprod_nh4(i,j,k) + cobalt%jremin_ndet(i,j,k) + cobalt%jremin_ndet_fast(i,j,k)
 
-            if (cobalt%do_external_source .and. k .eq. grid_kmt(i,j)) then
+            if (cobalt%do_external_source .and. k .eq. grid_kmt(i,j) .and. mask_addition_t(i,j,1) .gt. 0.0) then
                ! n_2_n_denit: mol NO3 consumed as OXIDANT per mol organic N remineralized.
                ! The NO3 comes from p_no3, not from f_ndet_kelp. Kelp N -> NH4 at 1:1;
                ! the 5.9 is dissolved nitrate destroyed to N2 and declared in net_srcn.
@@ -5871,7 +5871,6 @@ contains
             pre_totn(i,j,k)  = pre_totn(i,j,k) - e_juptake_no3(i,j,k)
             pre_totp(i,j,k)  = pre_totp(i,j,k) - e_juptake_po4(i,j,k)
             pre_totfe(i,j,k) = pre_totfe(i,j,k)- e_juptake_fed(i,j,k)
-            ! pre_totc(i,j,k)  = pre_totc(i,j,k) - cobalt%c_2_n*e_juptake_no3(i,j,k)
             pre_totc(i,j,k)  = pre_totc(i,j,k) - c_2_n_kelp*e_juptake_no3(i,j,k)  ! C:N is 9 for sugar kelp
 
       enddo; enddo ; enddo  !} i,j,k
@@ -6213,7 +6212,7 @@ contains
                pre_totn(i,j,k) = pre_totn(i,j,k) +cobalt%f_ndet_kelp(i,j)
                pre_totp(i,j,k) = pre_totp(i,j,k) + p_det_override(i,j) * dt
                pre_totfe(i,j,k) = pre_totfe(i,j,k) + fedet_override(i,j) *dt 
-               pre_totc(i,j,k) = pre_totc(i,j,k) + c_2_n_kelp * cobalt%f_ndet_kelp(i,j)
+               pre_totc(i,j,k) = pre_totc(i,j,k) + c_2_n_kelp * cobalt%jprod_nh4_kelp(i,j) *dt
                cobalt%f_ndet_kelp(i,j) = cobalt%f_ndet_kelp(i,j) - cobalt%jremin_ndet_kelp(i,j) * dt
             endif
          enddo; enddo !} i,j
@@ -6496,7 +6495,6 @@ contains
          if (cobalt%do_external_source) then
                if (k .eq. grid_kmt(i,j)) then
                   post_totn(i,j,k) = post_totn(i,j,k) + cobalt%f_ndet_kelp(i,j)
-                  ! post_totc(i,j,k) = post_totc(i,j,k) + c_2_n_kelp * cobalt%f_ndet_kelp(i,j)
                endif
          endif
 
@@ -6516,13 +6514,6 @@ contains
                     cobalt%p_nsmz(i,j,k,tau) + cobalt%p_nmdz(i,j,k,tau) + &
                     cobalt%p_nlgz(i,j,k,tau)))*grid_tmask(i,j,k)
 
-         ! DKSmod need to include p_ndet_kelp * c_2_n_kelp in post_totc
-         if (cobalt%do_external_source) then
-               if (k .eq. grid_kmt(i,j)) then
-                  ! post_totn(i,j,k) = post_totn(i,j,k) + cobalt%f_ndet_kelp(i,j)
-                  post_totc(i,j,k) = post_totc(i,j,k) + c_2_n_kelp * cobalt%f_ndet_kelp(i,j)
-               endif
-         endif
 
         imbal = (post_totc(i,j,k) - pre_totc(i,j,k) - net_srcc(i,j,k))*86400.0/dt*1.03e6
          if (abs(imbal).gt.imbalance_tolerance) then
